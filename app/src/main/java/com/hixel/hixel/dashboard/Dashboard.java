2087d7a8b3b87b70c8d66d27e9524fe59488679f
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-05-01 13:59:17 +1000

    Initial commit

---
+++ app/src/main/java/com/hixel/hixel/dashboard/Dashboard.java
@@ -0,0 +1,14 @@
+package com.hixel.hixel.dashboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hixel.hixel.R;
+
+public class Dashboard extends AppCompatActivity implements DashboardContract.View {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ app/src/main/java/com/hixel/hixel/dashboard/DashboardContract.java
@@ -0,0 +1,11 @@
+package com.hixel.hixel.dashboard;
+
+public interface DashboardContract {
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
