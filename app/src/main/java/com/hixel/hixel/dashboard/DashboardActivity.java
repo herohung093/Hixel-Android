6447b4ca8e7382cb39d1ac90d7f2f0e10d1bc948
BRAYDEN MARTIN KILLEEN <18531092@students.latrobe.edu.au>
2018-05-15 14:10:46 +1000

    simple search implementation

--- app/src/main/java/com/hixel/hixel/dashboard/DashboardActivity.java
+++ app/src/main/java/com/hixel/hixel/dashboard/DashboardActivity.java
@@ -1,38 +1,45 @@
 package com.hixel.hixel.dashboard;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 import android.widget.Spinner;
 
 import com.hixel.hixel.R;
 import com.hixel.hixel.databinding.ActivityDashboardBinding;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class DashboardActivity extends AppCompatActivity implements DashboardContract.View,
         OnItemSelectedListener {
 
-    private DashboardContract.Presenter mPresenter;
-    RecyclerView.Adapter mAdapter;
+    private DashboardContract.Presenter presenter;
+    RecyclerView.Adapter adapter;
+    ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityDashboardBinding binding =
-                DataBindingUtil.setContentView(this, R.layout.activity_dashboard);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard);
         setSupportActionBar(binding.toolbar.toolbar);
         binding.toolbar.toolbarTitle.setText(R.string.dashboard);
 
         // Init presenter
-        mPresenter = new DashboardPresenter(this);
-        mPresenter.start();
+        presenter = new DashboardPresenter(this);
+        presenter.start();
 
         Spinner spinner = binding.spinner;
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -41,28 +48,40 @@ public class DashboardActivity extends AppCompatActivity implements DashboardCon
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
-        RecyclerView mRecyclerView = binding.recyclerView;
-        mAdapter = new DashboardAdapter(this, mPresenter);
-        mRecyclerView.setAdapter(mAdapter);
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        RecyclerView recyclerView = binding.recyclerView;
+        this.adapter = new DashboardAdapter(this, presenter);
+        recyclerView.setAdapter(this.adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+
+        MaterialSearchView searchView = findViewById(R.id.search_view);
+        searchView.setMenuItem(item);
+
+        return true;
     }
 
     @Override
     public void setPresenter(@NonNull DashboardContract.Presenter presenter) {
-        mPresenter = presenter;
+        this.presenter = presenter;
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String item = parent.getItemAtPosition(position).toString();
-        mPresenter.sortCompaniesBy(item);
-        mAdapter.notifyDataSetChanged();
+        presenter.sortCompaniesBy(item);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
     public void portfolioChanged() {
-        mAdapter.notifyDataSetChanged();
+        adapter.notifyDataSetChanged();
     }
 
     @Override
