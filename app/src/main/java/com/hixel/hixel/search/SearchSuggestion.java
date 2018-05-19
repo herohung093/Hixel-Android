508c9eaed6d92aa7c7a9e999011629a2dc5e8bbd
naveengaur <naveengaur212@gmail.com>
2018-05-19 18:58:51 +1000

    Search Suggestion Back End

---
+++ app/src/main/java/com/hixel/hixel/search/SearchSuggestion.java
@@ -0,0 +1,26 @@
+package com.hixel.hixel.search;
+
+import java.util.ArrayList;
+
+public class SearchSuggestion {
+    private ArrayList<SearchEntry> mSearchEntries;
+
+
+    public SearchSuggestion() {
+    }
+
+    public void setSearchEntries(ArrayList<SearchEntry> searchEntries) {
+        mSearchEntries = searchEntries;
+    }
+
+    public ArrayList<String> getNames() {
+        ArrayList<String> names = new ArrayList<>();
+        for (int i = 0; i < mSearchEntries.size(); i++) {
+            names.add(mSearchEntries.get(i).getName() + "    " + mSearchEntries.get(i).getExchange() + ": " + mSearchEntries.get(i).getTicker());
+            }
+        return names;
+
+    }
+
+}
+
