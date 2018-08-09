5280b39065ea0accf59fb7d8140fd50a351629aa
BRAYDEN MARTIN KILLEEN <18531092@students.latrobe.edu.au>
2018-08-09 10:57:34 +1000

    (PTH-185) Tests for SearchEntry

---
+++ app/src/test/java/com/hixel/hixel/SearchEntryTest.java
@@ -0,0 +1,20 @@
+package com.hixel.hixel;
+
+
+import com.hixel.hixel.service.models.SearchEntry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SearchEntryTest {
+
+    @Test
+    public void testSearchClassCreation() {
+        SearchEntry searchEntry = new SearchEntry("ABC", "Apple", "NASDAQ");
+
+        assertEquals("Ticker value is incorrect", "ABC", searchEntry.getTicker());
+        assertEquals("Name value is incorrect", "Apple", searchEntry.getName());
+        assertEquals("Exchange value is incorrect", "NASDAQ", searchEntry.getExchange());
+    }
+
+}
