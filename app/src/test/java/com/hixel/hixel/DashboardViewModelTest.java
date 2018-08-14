aa59069e4700a1d3f1929916959e2b9d82f594a3
BRAYDEN MARTIN KILLEEN <18531092@students.latrobe.edu.au>
2018-08-14 13:38:23 +1000

    DashboardViewModel unit testing (PTH-182)

--- app/src/test/java/com/hixel/hixel/DashboardViewModelTest.java
+++ app/src/test/java/com/hixel/hixel/DashboardViewModelTest.java
@@ -1,23 +1,23 @@
 package com.hixel.hixel;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
 import com.hixel.hixel.service.models.Company;
 import com.hixel.hixel.viewmodel.DashboardViewModel;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class DashboardViewModelTest {
 
-    @Test
-    public void testGetPortfolio() {
-        DashboardViewModel dashboardViewModel = new DashboardViewModel();
 
-
-
-        dashboardViewModel.getPortfolio();
-
-
-    }
 }
