aeebc091e8916d06ad152095e7c51c6edd1cc8dd
BRAYDEN MARTIN KILLEEN <18531092@students.latrobe.edu.au>
2018-08-14 18:48:13 +1000

    Scaffolding for future tests (PTH-185)

--- app/src/test/java/com/hixel/hixel/ClientTest.java
+++ app/src/test/java/com/hixel/hixel/ClientTest.java
@@ -1,7 +1,24 @@
 package com.hixel.hixel;
 
-import org.junit.Test;
+import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import org.junit.Before;
+import org.junit.Rule;
+
+
+import org.mockito.MockitoAnnotations;
+
 
 public class ClientTest {
+    // Run on test thread
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
+    @Before
+    public void setup() {
+        // Inject mocks
+        MockitoAnnotations.initMocks(this);
+    }
+
+    // NOTE: No testing is required for this class yet, there is no logic to test.
+    // If this changes ensure that you use MockWebServer to avoid calls to the server.
 }
