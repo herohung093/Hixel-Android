b376b500a726325001281ac605ae2ca7884fb166
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-08-18 19:42:45 +1000

    Added a SingleLiveEvent that a SnackbarUtil can use to display information from the ViewModel

---
+++ app/src/main/java/com/hixel/hixel/util/SnackbarUtils.java
@@ -0,0 +1,15 @@
+package com.hixel.hixel.util;
+
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+public class SnackbarUtils {
+
+    public static void showSnackbar(View v, String snackbarText) {
+        if (v == null || snackbarText == null) {
+            return;
+        }
+
+        Snackbar.make(v, snackbarText, Snackbar.LENGTH_LONG).show();
+    }
+}
