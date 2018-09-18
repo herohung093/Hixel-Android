4e7a820fb97d1f5df1da7853603f4ae369f78c5c
herohung093 <hung.vm2574@gmail.com>
2018-09-18 10:04:09 +1000

    Transition CompanyCompare View to New UI

--- app/src/main/java/com/hixel/hixel/view/ui/GraphActivity.java
+++ app/src/main/java/com/hixel/hixel/view/ui/GraphActivity.java
@@ -1,70 +1,107 @@
 package com.hixel.hixel.view.ui;
 
+import android.app.Dialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import com.hixel.hixel.R;
-import com.hixel.hixel.databinding.ActivityGraphBinding;
 import com.hixel.hixel.service.models.Company;
-import com.hixel.hixel.view.ui.GraphFragment.OnFragmentInteractionListener;
 import com.hixel.hixel.viewmodel.GraphViewModel;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class GraphActivity extends FragmentActivity implements
-        AdapterView.OnItemSelectedListener, OnFragmentInteractionListener {
+        AdapterView.OnItemSelectedListener, GenericChartFragment.OnFragmentInteractionListener, GraphFragment.OnFragmentInteractionListener {
     private final String TAG = getClass().getSimpleName();
 
     ArrayList<String> ratios = new ArrayList<>();
     ArrayAdapter<String> listRatiosAdapter;
     Intent intentReceiver;
     Spinner listOfGraph;
+    TextView companyA, companyB;
     ArrayList<Company> receivedCompanies;
     GraphViewModel graphViewModel;
-    ActivityGraphBinding binding;
+    ImageView inforButton;
+
+
+   // ActivityGraphBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_graph);
+        setContentView(R.layout.activity_graph);
         intentReceiver = getIntent();
-
         receivedCompanies =
             (ArrayList<Company>) intentReceiver.getSerializableExtra("COMPARISON_COMPANIES");
 
+        Context context = this;
         //setup bottom navigator
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)binding.bottomNavGraph;
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.graph_generic_navigator);
         setupBottomNavigationView(bottomNavigationView);
         Log.d(TAG,"COMPANIES SIZE" + receivedCompanies.size());
+
         graphViewModel= ViewModelProviders.of(this).get(GraphViewModel.class);
         //Observe changes in list of ratios
         observeViewModel(graphViewModel);
 
+        companyA = findViewById(R.id.companyA);
+        companyA.setText(receivedCompanies.get(0).getIdentifiers().getName());
+        companyB = findViewById(R.id.companyB);
+        companyB.setText(receivedCompanies.get(1).getIdentifiers().getName());
+
+        inforButton = findViewById(R.id.imageView3);
+        inforButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog= new Dialog(context);
+                dialog.setContentView(R.layout.information_popup_window);
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                dialog.setTitle("Title...");
+                int width = (int)(getResources().getDisplayMetrics().widthPixels*0.90);
+                int height = (int)(getResources().getDisplayMetrics().heightPixels*0.90);
+                dialog.getWindow().setLayout(width,height);
+                ImageView close_ib =dialog.findViewById(R.id.popup_ib_close);;
+                close_ib.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view){
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
     }
 
-    public void setupListOfRatios(ArrayList<String> sprinnerList) {
-        listRatiosAdapter =  new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sprinnerList);
+    public void setupListOfRatios(ArrayList<String> spinnerList) {
+        listRatiosAdapter =  new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinnerList);
         listRatiosAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        listOfGraph = binding.spinner;
+        listOfGraph = findViewById(R.id.spinner2);
         listOfGraph.setOnItemSelectedListener(this);
         listOfGraph.setAdapter(listRatiosAdapter);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        GraphFragment FragmentA =
-                (GraphFragment) getFragmentManager().findFragmentById(R.id.graphFragment);
-         FragmentA.drawGraph(receivedCompanies, adapterView.getSelectedItem().toString());
+        GraphFragment fragmentA =
+                (GraphFragment) getFragmentManager().findFragmentById(R.id.fragment_bar_char);
+        fragmentA.drawGraph(receivedCompanies, adapterView.getSelectedItem().toString());
+        GenericChartFragment fragmentB =
+            (GenericChartFragment) getFragmentManager().findFragmentById(R.id.fragment_radar_chart);
+        fragmentB.drawGraph(receivedCompanies);
     }
 
     @Override
@@ -122,4 +159,6 @@ public class GraphActivity extends FragmentActivity implements
             }
         });
     }
+
+
 }
--- app/src/main/java/com/hixel/hixel/view/ui/GraphFragment.java
+++ app/src/main/java/com/hixel/hixel/view/ui/GraphFragment.java
@@ -7,17 +7,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.hixel.hixel.R;
 import com.hixel.hixel.service.models.Company;
 import com.hixel.hixel.service.models.FinancialData;
@@ -28,12 +29,11 @@ import java.util.List;
 
 public class GraphFragment extends Fragment {
 
-    LineChart lineChart;
+    private CombinedChart mChart;
     String[] years;
     ArrayList<Integer> compAColors= new ArrayList<Integer>();
     ArrayList<Integer> compBColors= new ArrayList<Integer>();
     private OnFragmentInteractionListener mListener;
-
     public GraphFragment() {
         // Required empty public constructor
     }
@@ -43,13 +43,16 @@ public class GraphFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_graph, container, false);
-        lineChart = view.findViewById(R.id.chart1);
+        mChart =  view.findViewById(R.id.chart1);
         return view;
     }
     public LineDataSet lineChartDataSetup( String selectedRatio, Company company){
@@ -67,6 +70,21 @@ public class GraphFragment extends Fragment {
         }
 
         return new LineDataSet(compEntry,company.getIdentifiers().getTicker());
+    }
+
+    public BarDataSet barChartDataSetup(String selectedRatio, Company company){
+        List<BarEntry> compEntry = new ArrayList<>();
+        List<FinancialData> financialData= company.getFinancialDataEntries();
+        checkYearNull(financialData);
+        createListOfYears(financialData);
+        int j=4;
+        for (int i=0;i<5;i++){
+            LinkedHashMap<String, Double> DataCompAYear1 = financialData.get(j).getRatios();
+            j--;
+            BarEntry compYearData = new BarEntry(i, Float.valueOf(DataCompAYear1.get(selectedRatio).toString()));
+            compEntry.add(compYearData);
+        }
+        return new BarDataSet(compEntry,company.getIdentifiers().getTicker());
     }
     public void colorIndicator(Company company, String selectedRatio, ArrayList<Integer> colors){
         ArrayList<Float> rawData= new ArrayList<>();
@@ -96,43 +114,44 @@ public class GraphFragment extends Fragment {
 
     }
     public void drawGraph(ArrayList<Company> companies,String selectedRatio){
-        List<ILineDataSet> dataSets = new ArrayList<>();
 
-        for(Company c: companies){
-            LineDataSet setCompA= lineChartDataSetup( selectedRatio, c);
-                if( companies.indexOf(c)==0) {
-                colorIndicator(c,selectedRatio,compAColors);
-                setCompA.setColors(compAColors);
-                setupDatasetStyle(setCompA,companies.get(0).getFinancialDataEntries());
-                setCompA.enableDashedLine(10f, 10f, 10f);
-                dataSets.add(setCompA);
-            } else {
-                colorIndicator(c,selectedRatio,compBColors);
-                setCompA.setColors(compBColors);
-                setupDatasetStyle(setCompA,companies.get(1).getFinancialDataEntries());
-                dataSets.add(setCompA);
-            }
+        LineData lineData = new LineData();
+        LineDataSet setCompA= lineChartDataSetup( selectedRatio, companies.get(0));
+        lineData.addDataSet(setCompA);
+        LineDataSet setCompB = lineChartDataSetup(selectedRatio, companies.get(1));
+        //BarDataSet setCompB = barChartDataSetup(selectedRatio, companies.get(1));
+        //BarData barData = new BarData(setCompB);
+        lineData.addDataSet(setCompB);
+        ///barData.setBarWidth(0.45f);
+        //colorIndicator(companies.get(0),selectedRatio,compAColors);
 
-        }
-        Description description=lineChart.getDescription();
-        description.setEnabled(false);
-        LineData data = new LineData(dataSets);
-        decorLineChart(lineChart,data);
+        setupDatasetStyle(setCompA,setCompB);//
+        //draw graph
+        mChart.setDrawOrder(new CombinedChart.DrawOrder[]{
+            CombinedChart.DrawOrder.BAR,  CombinedChart.DrawOrder.LINE
+        });
+        decorLineChart(mChart);
+
+        CombinedData data = new CombinedData();
+
+        data.setData( lineData);
+        //data.setData(barData);
+        mChart.getDescription().setEnabled(false);
+        mChart.setData(data);
+        mChart.invalidate();
     }
 
-    public void decorLineChart(LineChart lineChart, LineData data){
+    public void decorLineChart(CombinedChart mchart){
 
-        lineChart.animateXY(1000, 1000);
-        lineChart.setData(data);
-        lineChart.invalidate();
-        XAxis xAxis = lineChart.getXAxis();
+        mchart.animateXY(1000, 1000);
+        mchart.setDrawGridBackground(false);
+        XAxis xAxis = mchart.getXAxis();
 
-        YAxis yAxis = lineChart.getAxisLeft();
-        YAxis yRight = lineChart.getAxisRight();
-        yRight.setTextColor(Color.GRAY);
+        YAxis yAxis = mchart.getAxisLeft();
+
         setupAxis(xAxis, yAxis);
 
-        Legend legend = lineChart.getLegend();
+        Legend legend = mchart.getLegend();
         setupLegend(legend);
     }
 
@@ -165,18 +184,26 @@ public class GraphFragment extends Fragment {
         years = toConvertYears.toArray(new String[toConvertYears.size()]);
     }
 
-    public void setupDatasetStyle(LineDataSet setCompA, List<FinancialData> financialData) {
+    public void setupDatasetStyle(LineDataSet setCompA, LineDataSet setCompB) {//
+        //Line
         setCompA.setDrawCircleHole(true);
         setCompA.setValueTextSize(12);
         setCompA.setValueTextColor(Color.GRAY);
-
         setCompA.setCircleHoleRadius(3);
         setCompA.setAxisDependency(YAxis.AxisDependency.LEFT);
+        setCompA.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        setCompA.setAxisDependency(YAxis.AxisDependency.LEFT);
+        setCompA.setColor(Color.rgb(229, 13, 92));
+        //Bar
+        setCompB.setColors(Color.rgb(47, 237, 208));
+        setCompB.setValueTextColor(Color.rgb(60, 220, 78));
+        setCompB.setValueTextSize(10f);
+        setCompB.setAxisDependency(YAxis.AxisDependency.LEFT);
 
     }
 
     public void setupLegend(Legend legend) {
-        legend.setEnabled(true);
+/*        legend.setEnabled(true);
         legend.setOrientation(Legend.LegendOrientation.VERTICAL);
         legend.setFormSize(7f); // set the size of the legend forms/shapes
         legend.setForm(Legend.LegendForm.CIRCLE); // set what type of form/shape should be used
@@ -187,16 +214,24 @@ public class GraphFragment extends Fragment {
         legend.setXEntrySpace(55); // set the space between the legend entries on the x-axis
         legend.setYEntrySpace(5f); // set the space between the legend entries on the y-axis
         // set custom labels and colors
+        */
+        legend.setWordWrapEnabled(true);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
     }
 
     public void setupAxis(XAxis xAxis, YAxis yAxis) {
         xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setAxisMinimum(0f);
         xAxis.setValueFormatter(formatter);
         xAxis.setTextSize(12f);
         xAxis.setTextColor(Color.BLACK);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
 
         yAxis.setTextSize(12f);
         yAxis.setTextColor(Color.BLACK);
+
     }
 
     @Override
