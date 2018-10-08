dbe3b24d86e099eaf363f3b2f0ba68ce5443bffb
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-08 19:28:59 +1100

    Setup user repository

---
+++ app/src/main/java/com/hixel/hixel/data/database/UserDao.java
@@ -0,0 +1,24 @@
+package com.hixel.hixel.data.database;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import com.hixel.hixel.data.entities.User;
+import java.util.Date;
+
+@Dao
+public interface UserDao {
+
+    @Insert(onConflict = REPLACE)
+    void saveUser(User user);
+
+    @Query("SELECT * FROM user WHERE email = :userEmail AND password = :userPassword")
+    LiveData<User> getUser(String userEmail, String userPassword);
+
+    @Query("SELECT * FROM user WHERE email = :userEmail AND lastRefresh > :lastRefreshMax LIMIT 1")
+    int hasUser(String userEmail, Date lastRefreshMax);
+
+}
