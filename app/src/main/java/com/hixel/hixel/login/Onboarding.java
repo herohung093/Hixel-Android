1b2d9e3ba692eb2eff1e6cf7ec23d04b9b500aa4
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-08 13:08:32 +1100

    New onboarding UI

--- app/src/main/java/com/hixel/hixel/login/Onboarding.java
+++ app/src/main/java/com/hixel/hixel/login/Onboarding.java
@@ -1,69 +1,69 @@
 package com.hixel.hixel.login;
 
 import android.content.Intent;
-import android.support.v4.view.ViewPager;
-import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderCard;
 import com.hixel.hixel.R;
-import com.hixel.hixel.commonui.SliderAdapter;
 
-public class Onboarding extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private LinearLayout linearLayout;
-    private Button signupButton;
+public class Onboarding extends AhoyOnboarderActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        linearLayout = findViewById(R.id.linearLayout);
+        List<Integer> colorList = new ArrayList<>();
+        colorList.add(R.color.onboarding_green);
+        colorList.add(R.color.onboarding_blue);
+        colorList.add(R.color.onboarding_yellow);
+        setColorBackground(colorList);
 
-        SliderAdapter sliderAdapter = new SliderAdapter(this);
+        // Available properties
+        // Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+        // setFont(face);
 
-        viewPager.setAdapter(sliderAdapter);
-        viewPager.addOnPageChangeListener(viewListener);
+        // Set pager indicator colors
+        // setInactiveIndicatorColor(R.color.grey);
+        // setActiveIndicatorColor(R.color.white);
 
+        // Set finish button text
+        setFinishButtonTitle("Get Started");
+
+        // Set the finish button style
+        // setFinishButtonDrawableStyle(ContextCompat.getDrawable(this, R.drawable.rounded_button));
+
+        setOnboardPages(getOnBoardingCards());
     }
 
-    public void addDotsIndicator(int position) {
-
-        TextView[] dots = new TextView[3];
-        linearLayout.removeAllViews();
-
-        for (int i = 0; i < dots.length; i++) {
-            dots[i] = new TextView(this);
-            dots[i].setText(R.string.dot);
-            dots[i].setTextSize(50);
-            dots[i].setTextColor(getColor(R.color.primary_background));
-
-            linearLayout.addView(dots[i]);
-        }
-
-        dots[position].setTextColor(getColor(R.color.good));
-    }
-
-    private void openSignup() {
+    @Override
+    public void onFinishButtonPressed() {
         Intent moveToSignup = new Intent(getApplicationContext(), SignupActivity.class);
         startActivity(moveToSignup);
     }
 
-    ViewPager.OnPageChangeListener viewListener = new OnPageChangeListener() {
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) { }
+    public List<AhoyOnboarderCard> getOnBoardingCards() {
 
-        @Override
-        public void onPageSelected(int position) {
-            addDotsIndicator(position);
-        }
+        List<AhoyOnboarderCard> pages = new ArrayList<>();
+        pages.add(new AhoyOnboarderCard("Title",
+                "Description",
+                R.drawable.ic_action_action_search));
+        pages.add(new AhoyOnboarderCard("Title", "Description", R.drawable.ic_action_action_search));
+        pages.add(new AhoyOnboarderCard("Title", "Description", R.drawable.ic_action_action_search));
 
-        @Override
-        public void onPageScrollStateChanged(int state) { }
-    };
+        // TODO: Styling.
+        /*
+            card1.setBackgroundColor(R.color.black_transparent);
+            card1.setTitleColor(R.color.white);
+            card1.setDescriptionColor(R.color.grey_200);
+            card1.setTitleTextSize(dpToPixels(10, this));
+            card1.setDescriptionTextSize(dpToPixels(8, this));
+            card1.setIconLayoutParams(iconWidth, iconHeight, marginTop, marginLeft, marginRight, marginBottom);
+         */
+
+        return pages;
+    }
 }
