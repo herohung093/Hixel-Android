99d273b83dd54274a593e585455622d391ec45b5
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-08 20:53:48 +1100

    Implemented user repository

---
+++ app/src/main/java/com/hixel/hixel/login/LoginViewModel.java
@@ -0,0 +1,31 @@
+package com.hixel.hixel.login;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import com.hixel.hixel.data.UserRepository;
+import com.hixel.hixel.data.entities.User;
+import javax.inject.Inject;
+
+public class LoginViewModel extends ViewModel {
+
+    private LiveData<User> user;
+    private UserRepository repository;
+    private boolean isUserStale;
+
+    @Inject
+    LoginViewModel(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void init() {
+        isUserStale = repository.isUserStale();
+    }
+
+    public boolean getIsUserStale() {
+        return isUserStale;
+    }
+
+    public boolean login(String email, String password) {
+        return repository.verifyUser(email, password);
+    }
+}
