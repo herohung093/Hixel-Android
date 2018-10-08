9ef6841d6d1afc9f2e3f69000a350ef7899daaba
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-08 16:20:02 +1100

    Onboarding styling

--- app/src/main/java/com/hixel/hixel/login/Onboarding.java
+++ app/src/main/java/com/hixel/hixel/login/Onboarding.java
@@ -3,14 +3,16 @@ package com.hixel.hixel.login;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import android.support.v4.content.ContextCompat;
+import com.chyrta.onboarder.OnboarderActivity;
+import com.chyrta.onboarder.OnboarderPage;
 import com.codemybrainsout.onboarder.AhoyOnboarderCard;
 import com.hixel.hixel.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Onboarding extends AhoyOnboarderActivity {
+public class Onboarding extends OnboarderActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +21,15 @@ public class Onboarding extends AhoyOnboarderActivity {
         List<Integer> colorList = new ArrayList<>();
         colorList.add(R.color.onboarding_green);
         colorList.add(R.color.onboarding_blue);
-        colorList.add(R.color.onboarding_yellow);
-        setColorBackground(colorList);
+        colorList.add(R.color.onboarding_alt_orange);
+        // setColorBackground(colorList);
 
         // Available properties
-        // Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
-        // setFont(face);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Raleway-Regular.ttf");
+        setFont(face);
+
+        // Hide navigation controls
+        showNavigationControls(false);
 
         // Set pager indicator colors
         // setInactiveIndicatorColor(R.color.grey);
@@ -34,7 +39,7 @@ public class Onboarding extends AhoyOnboarderActivity {
         setFinishButtonTitle("Get Started");
 
         // Set the finish button style
-        // setFinishButtonDrawableStyle(ContextCompat.getDrawable(this, R.drawable.rounded_button));
+        setFinishButtonDrawableStyle(ContextCompat.getDrawable(this, R.drawable.onboarding_button_style));
 
         setOnboardPages(getOnBoardingCards());
     }
@@ -45,25 +50,36 @@ public class Onboarding extends AhoyOnboarderActivity {
         startActivity(moveToSignup);
     }
 
-    public List<AhoyOnboarderCard> getOnBoardingCards() {
+    public List<OnboarderPage> getOnBoardingCards() {
 
-        List<AhoyOnboarderCard> pages = new ArrayList<>();
-        pages.add(new AhoyOnboarderCard("Title",
-                "Description",
-                R.drawable.ic_action_action_search));
-        pages.add(new AhoyOnboarderCard("Title", "Description", R.drawable.ic_action_action_search));
-        pages.add(new AhoyOnboarderCard("Title", "Description", R.drawable.ic_action_action_search));
+        List<AhoyOnboarderCard> cards = new ArrayList<>();
+        cards.add(new AhoyOnboarderCard(
+                "No crazy analysis!",
+                "We use easy to understand charts, and assign a company a score out of 100.",
+                R.drawable.onboarding_alternate_barchart)
+        );
 
-        // TODO: Styling.
+        cards.add(new AhoyOnboarderCard(
+                "Companies you care about.",
+                "Add and remove companies as you like, and compare them to see whose best.",
+                R.drawable.onboarding_compare)
+        );
+
+        cards.add(new AhoyOnboarderCard(
+                "Let's Go!",
+                "Sign up now and make your own portfolio in minutes!",
+                R.drawable.onboarding_launch)
+        );
+
         /*
+            card.setTitleTextSize(dpToPixels(10, this));
+            card1.setDescriptionTextSize(dpToPixels(8, this));
             card1.setBackgroundColor(R.color.black_transparent);
             card1.setTitleColor(R.color.white);
             card1.setDescriptionColor(R.color.grey_200);
-            card1.setTitleTextSize(dpToPixels(10, this));
-            card1.setDescriptionTextSize(dpToPixels(8, this));
             card1.setIconLayoutParams(iconWidth, iconHeight, marginTop, marginLeft, marginRight, marginBottom);
          */
 
-        return pages;
+        return cards;
     }
 }
