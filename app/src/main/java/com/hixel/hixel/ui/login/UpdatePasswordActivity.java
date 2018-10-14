33296f6402750321c5f4b7a2fd3cd29e999225e5
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-15 02:07:05 +1100

    Refactored, databinding, viewModel

--- app/src/main/java/com/hixel/hixel/ui/login/UpdatePasswordActivity.java
+++ app/src/main/java/com/hixel/hixel/ui/login/UpdatePasswordActivity.java
@@ -1,20 +1,34 @@
 package com.hixel.hixel.ui.login;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Toast;
 import com.hixel.hixel.R;
+import com.hixel.hixel.databinding.ActivityUpdatePasswordBinding;
+import dagger.android.AndroidInjection;
+import javax.inject.Inject;
 
 public class UpdatePasswordActivity extends AppCompatActivity {
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
+    private LoginViewModel viewModel;
+
+    ActivityUpdatePasswordBinding binding;
+
     TextInputLayout newPassTV, confirmPassTV;
     Button changePassButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_password);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_update_password);
 
         changePassButton= findViewById(R.id.changePassButton);
         newPassTV= findViewById(R.id.forgotView_PassWrapper);
@@ -31,11 +45,18 @@ public class UpdatePasswordActivity extends AppCompatActivity {
         });
     }
 
+    private void configureDagger() {
+        AndroidInjection.inject(this);
+    }
+
+    private void configureViewModel() {
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(LoginViewModel.class);
+    }
+
     public boolean validate() {
         boolean valid = true;
 
-        if (newPassTV.getEditText().getText().toString()
-            .compareTo(confirmPassTV.getEditText().getText().toString())!=0){
+        if (newPassTV.getEditText().getText().toString().compareTo(confirmPassTV.getEditText().getText().toString())!=0){
             valid=false;
             Toast.makeText(getBaseContext(), "Your passwords do not match", Toast.LENGTH_LONG).show();
         }
