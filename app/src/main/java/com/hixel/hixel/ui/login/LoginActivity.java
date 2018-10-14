1e50d59a5b07de97fdf778dfbf55090c8b405d25
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-14 16:01:19 +1100

    Login back to start

--- app/src/main/java/com/hixel/hixel/ui/login/LoginActivity.java
+++ app/src/main/java/com/hixel/hixel/ui/login/LoginActivity.java
@@ -6,21 +6,28 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
-import android.databinding.ObservableBoolean;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.Toast;
+import com.hixel.hixel.App;
 import com.hixel.hixel.R;
 import com.hixel.hixel.dashboard.DashboardActivity;
+import com.hixel.hixel.data.api.Client;
+import com.hixel.hixel.data.api.ServerInterface;
+import com.hixel.hixel.data.models.LoginData;
 import com.hixel.hixel.databinding.ActivityLoginBinding;
 import com.hixel.hixel.login.ForgotPasswordActivity;
 import dagger.android.AndroidInjection;
 import javax.inject.Inject;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -62,7 +69,6 @@ public class LoginActivity extends AppCompatActivity {
     private void configureViewModel() {
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(LoginViewModel.class);
         viewModel.init();
-
         setupUI();
     }
 
@@ -87,7 +93,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login() {
-
         if (!validate()) {
             onLoginFailed("Invalid input");
             return;
@@ -95,23 +100,54 @@ public class LoginActivity extends AppCompatActivity {
 
         loginButton.setEnabled(false);
 
-        // final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
-        // progressDialog.getWindow().setGravity(Gravity.CENTER);
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.getWindow().setGravity(Gravity.CENTER);
 
-        // progressDialog.setIndeterminate(true);
-        // progressDialog.setMessage("Authenticating...");
-        // progressDialog.show();
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
 
+        String email = emailText.getEditText().getText().toString();
+        String password = passwordText.getEditText().getText().toString();
 
-        String email = emailText.getEditText().getText().toString().trim();
-        String password = passwordText.getEditText().getText().toString().trim();
-        viewModel.verifyUser(email, password);
+        Call<Void> call = Client.getClient()
+                .create(ServerInterface.class)
+                .login(new LoginData(email, password));
 
-        if (viewModel.getIsValidUser()) {
-            onLoginSuccess();
-        } else {
-            onLoginFailed("TODO");
-        }
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call,
+                    @NonNull Response<Void> response) {
+                progressDialog.dismiss();
+                switch (response.code()) {
+                    case 200:
+                        SharedPreferences preferences = App.preferences();
+
+                        preferences.edit()
+                                .putString("AUTH_TOKEN", response.headers().get("Authorization"))
+                                .apply();
+
+                        preferences.edit()
+                                .putString("REFRESH_TOKEN", response.headers().get("Refresh"))
+                                .apply();
+
+                        onLoginSuccess();
+                        break;
+                    case 401:
+                        onLoginFailed("Wrong username or password");
+                        break;
+                    default:
+                        onLoginFailed("Unknown error");
+                        break;
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+                progressDialog.dismiss();
+                onLoginFailed("Couldn't connect to server!");
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/hixel/hixel/ui/login/LoginViewModel.java
+++ app/src/main/java/com/hixel/hixel/ui/login/LoginViewModel.java
@@ -1,11 +1,8 @@
 package com.hixel.hixel.ui.login;
-
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
-import android.content.SharedPreferences;
-import android.databinding.ObservableBoolean;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import com.hixel.hixel.data.UserRepository;
 import com.hixel.hixel.data.api.Client;
 import com.hixel.hixel.data.api.ServerInterface;
@@ -18,19 +15,32 @@ import retrofit2.Response;
 
 public class LoginViewModel extends ViewModel {
 
+    private static final String TAG = LoginViewModel.class.getSimpleName();
+
     private LiveData<User> user;
     private UserRepository repository;
 
-    private boolean isValidUser = false;
+    private boolean isValidUser;
+    private MutableLiveData<Boolean> is;
+    private LiveData<Boolean> x;
 
     @Inject
     LoginViewModel(UserRepository repository) {
         this.repository = repository;
     }
 
-    public void init() { }
+    void init() {
+        is = new MutableLiveData<>();
+        is.setValue(true);
+        x = is;
+    }
 
-    public void verifyUser(String email, String password) {
+    LiveData<Boolean> getX() {
+        return x;
+    }
+
+
+    void verifyUser(String email, String password) {
         Call<Void> call = Client.getClient()
                 .create(ServerInterface.class)
                 .login(new LoginData(email, password));
@@ -41,32 +51,34 @@ public class LoginViewModel extends ViewModel {
                     @NonNull Response<Void> response) {
                 switch (response.code()) {
                     case 200:
-                        loginSuccess();
+                        is.setValue(true);
+                        setIsValidUser(true);
                         break;
                     case 401:
-                        loginFailed();
                         break;
                     default:
-                        loginFailed();
                         break;
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+
             }
         });
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+
+        }
     }
 
     public boolean getIsValidUser() {
         return isValidUser;
     }
 
-    public void loginSuccess() {
-        isValidUser = true;
+    public void setIsValidUser(boolean isValid) {
+        isValidUser = isValid;
     }
 
-    public void loginFailed() {
-        isValidUser = false;
-    }
 }
