bbd66d1d64e589fa170f557c6c34ad9cf92e5b19
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-14 18:14:03 +1100

    Updated model to deal with portfolio

---
+++ app/src/main/java/com/hixel/hixel/data/entities/Portfolio.java
@@ -0,0 +1,20 @@
+package com.hixel.hixel.data.entities;
+
+import android.arch.persistence.room.TypeConverters;
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class Portfolio {
+
+    @SerializedName("companies")
+    @TypeConverters(PortfolioTypeConverter.class)
+    private List<String> companies;
+
+    public List<String> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(List<String> companies) {
+        this.companies = companies;
+    }
+}
