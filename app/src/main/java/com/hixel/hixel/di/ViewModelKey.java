bbb9f136056581beac0270b334d2144ca20b1d85
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-17 07:10:55 +1100

    Documentation

--- app/src/main/java/com/hixel/hixel/di/ViewModelKey.java
+++ app/src/main/java/com/hixel/hixel/di/ViewModelKey.java
@@ -1,13 +1,20 @@
 package com.hixel.hixel.di;
 
 import android.arch.lifecycle.ViewModel;
+
 import dagger.MapKey;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Creates a map of ViewModel class types and ViewModel instances to be passed to the
+ * ViewModelFactory. This is used so we only have to have one ViewModelFactory, checkout Dagger2
+ * multibindings.
+ */
 @Documented
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
