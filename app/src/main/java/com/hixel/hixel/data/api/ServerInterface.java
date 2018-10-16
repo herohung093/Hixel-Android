7ebd05424941e61de7b0cd94ae8c2b5797f12e3e
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-16 13:37:48 +1100

    Jonah's added API endpoints

--- app/src/main/java/com/hixel/hixel/data/api/ServerInterface.java
+++ app/src/main/java/com/hixel/hixel/data/api/ServerInterface.java
@@ -31,8 +31,23 @@ public interface ServerInterface {
     @Headers("No-Authentication: true")
     Call<Void> signup(@Body ApplicationUser request);
 
+    @GET("/users/reset-email")
+    @Headers("No-Authentication: true")
+    Call<Void> resetEmail(@Query("email") String email);
+
     @GET("/users/profile")
     Call<User> userData();
+
+    @GET("/users/reset-code")
+    @Headers("No-Authentication: true")
+    Call<Void> resetCode(@Query("email") String email, @Query("code") String code);
+
+    @GET("/users/reset-password")
+    @Headers("No-Authentication: true")
+    Call<Void> resetPassword(@Query("email") String email, @Query("code") String code, @Query("password") String password);
+
+    @GET("/users/change-password")
+    Call<Void> changePassword(@Query("code") String oldPassword, @Query("password") String newPassword);
 
     @GET("/users/refresh")
     Call<Void> refreshAccessToken(@Header("Refresh") String Refresh);
