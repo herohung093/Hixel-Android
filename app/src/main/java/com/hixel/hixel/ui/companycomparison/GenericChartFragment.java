6c590f7b7672aafa4d56e58b6feda1a430da63d9
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-17 03:11:35 +1100

    Removed onInteractionListener

--- app/src/main/java/com/hixel/hixel/ui/companycomparison/GenericChartFragment.java
+++ app/src/main/java/com/hixel/hixel/ui/companycomparison/GenericChartFragment.java
@@ -26,7 +26,6 @@ import java.util.List;
 
 public class GenericChartFragment extends Fragment {
 
-    private OnFragmentInteractionListener listener;
     private RadarChart radarChart;
 
     public GenericChartFragment() { }
@@ -145,25 +144,10 @@ public class GenericChartFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            listener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        listener = null;
     }
-
-    /**
-     * This interface must be implemented by activities that contain this fragment to allow an
-     * interaction in this fragment to be communicated to the activity and potentially other
-     * fragments contained in that activity. <p> See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html" >Communicating
-     * with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener { }
 }
